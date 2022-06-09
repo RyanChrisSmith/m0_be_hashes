f9dfdf1c0a36fb675063f88c8400a8a203094878
Ryan Smith <ryan.chris.smith@gmail.com>
2022-06-09 14:15:23 -0600

    Initial commit

--- hashes_2.rb
+++ hashes_2.rb
@@ -8,7 +8,9 @@ states = {
 }
 
 # Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
-#
+states.each do |state|
+  p state
+end
 #
 #
 
@@ -24,7 +26,9 @@ birthdays = {
 }
 
 # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
-#
+birthdays.each do |birthday|
+  p birthday
+end
 #
 #
 
@@ -40,6 +44,10 @@ login_statuses = {
 }
 
 # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
-#
+login_statuses.each do |name, status|
+  if status == true
+    puts name + " you are logged in."
+  end
+end
 #
 #
